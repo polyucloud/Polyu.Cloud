20e68a7bcf89ab095c83c7393cc84d35f0a5636f
Polyu.Cloud <polyu.cloud@gmail.com>
2014-04-14 12:49:13 +0800

    opopop

--- app/src/main/java/com/example/polyucloud/app/ListContent.java
+++ app/src/main/java/com/example/polyucloud/app/ListContent.java
@@ -12,7 +12,6 @@ public class ListContent extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_content);
-        //..............
     }
 
 
