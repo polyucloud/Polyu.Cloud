dc71df6cdab01ce4429a4789c92c585b153238fe
Polyu.Cloud <polyu.cloud@gmail.com>
2014-04-14 12:34:02 +0800

    hi

--- app/src/main/java/com/example/polyucloud/app/ListContent.java
+++ app/src/main/java/com/example/polyucloud/app/ListContent.java
@@ -12,6 +12,7 @@ public class ListContent extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_content);
+        //vvv
     }
 
 
